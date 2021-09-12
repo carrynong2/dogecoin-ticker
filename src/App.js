f42bfae3d7356775253cf305bfe6b3de2a56f586
carrynong2 <carrynong2@gmail.com>
2021-09-12 23:54:18 +0700

    ui responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import PriceCard from "./components/PriceCard";
 import logo from "./logo.png";
+import "./App.css";
 
-function App() {
+const App = () => {
   const [ticker, setTicker] = useState({
     low: 0,
     high: 0,
@@ -19,10 +19,7 @@ function App() {
       setTicker(data.ticker);
     }
     getDogecoinPrice();
-    const interval = setInterval(() => getDogecoinPrice(), 10000);
-    return () => {
-      clearInterval(interval);
-    };
+    setInterval(() => getDogecoinPrice(), 10000);
   }, []);
 
   return (
@@ -36,11 +33,11 @@ function App() {
         <PriceCard type="current" price={ticker.last} />
       </div>
       <p>
-        Dogecoin price updated every 10 seconds seconds from{" "}
+        Dogecoin price updated every 10 seconds from{" "}
         <a href="https://wazirx.com/">WazirX API</a>
       </p>
     </div>
   );
-}
+};
 
 export default App;
